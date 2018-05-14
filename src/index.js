b6b4f2a9ad4b5df0516081f58a59edd822a135d4
marcos012 <marcospaulo.ad@gmail.com>
2018-05-14 08:46:09 -0300

    :rocket: <@marcos012, @claudiastrm, @murilobauerc> começa configuração circle express

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send
+})
+
+//continuar tutorial
+//https://medium.com/@yoobi55/setting-up-heroku-server-with-node-js-express-eng-b0c8cc61a2a9
