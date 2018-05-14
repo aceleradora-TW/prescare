6ed1b86e71eca6ba98d8d94879963c08cd2866c0
murilobauerc <panchobauer@gmail.com>
2018-05-14 09:16:26 -0300

    :art: <@murilobauerc, @marcos012> inicia servidor local heroku

--- src/index.js
+++ src/index.js
@@ -9,6 +9,3 @@ app.get('/', (req, res) => {
 
 app.listen(app.get('port'),() =>
     console.log("Servidor rodando na porta: " + app.get('port')))
-
-//continuar tutorial
-//https://medium.com/@yoobi55/setting-up-heroku-server-with-node-js-express-eng-b0c8cc61a2a9
