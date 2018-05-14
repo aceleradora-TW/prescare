c9171da2b8a89017ff90fe1b03ce03e769efb164
murilobauerc <panchobauer@gmail.com>
2018-05-14 09:36:54 -0300

    :art: <@murilobauerc, @marcos012, @claudiastrm> renomeia arquivo para app.js

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ const app = express()
 app.set('port', (process.env.PORT || 3000))
 
 app.get('/', (req, res) => {
-    res.send('Ola')
+  res.send('Bye Aceleradora!!!')
 })
 
-app.listen(app.get('port'),() =>
-    console.log("Servidor rodando na porta: " + app.get('port')))
+app.listen(app.get('port'), () =>
+  console.log("Servidor rodando na porta: "+app.get('port')))
 
 //continuar tutorial
 //https://medium.com/@yoobi55/setting-up-heroku-server-with-node-js-express-eng-b0c8cc61a2a9
