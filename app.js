a671159b41d1be73c329e7394f94dacf9fbb77ce
EngelFlores <engel.tads@gmail.com>
2018-05-14 14:18:19 -0300

    :art: <@Claudiastrm, @EngelFlores> Cria arquivo separado para user

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const express = require('express')
 const app = express()
 const expressLayouts = require ('express-ejs-layouts')
+const user = require('./public/js/user')
 const port = 3000
 
 app.set('port', (process.env.PORT || 3000))
-
 app.set('view engine','ejs')
+
 app.use(expressLayouts)
-app.get('/',(req,res)=>{
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
   res.render('home')
 })
-app.get('/about',(req,res)=>{
-  const user = {
-    name: 'Márcia',
-    email: 'marcia@gmail.com',
-    avatar: 'https://pbs.twimg.com/profile_images/715629565146767363/OjnVtVFN_400x400.jpg'
-  }
+app.get('/about', (req, res) => {
   res.render('about', { usuario: user })
 })
-app.use(express.static(__dirname + '/public'))
+
 app.listen(port)
-console.log('Servidor iniciado em http://localhost:'+ port)
+console.log('Servidor iniciado em http://localhost:' + port)
---
+++ public/js/user.js
@@ -0,0 +1,7 @@
+const user = {
+  name: 'Márcia',
+  email: 'marcia@gmail.com',
+  avatar: 'https://pbs.twimg.com/profile_images/715629565146767363/OjnVtVFN_400x400.jpg'
+}
+
+module.exports = user
