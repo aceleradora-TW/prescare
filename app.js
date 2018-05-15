5d4a1d314b7e0c1712c0f455b30a543492e95ca3
alineabich <aline.abich@acad.pucrs.br>
2018-05-15 10:28:13 -0300

    :rocket: <@alineabich, @claudiastrm> configura ejs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const expressLayouts = require ('express-ejs-layouts')
 const ejs = require('ejs')
+const user = require('./public/js/user')
 
 app.set('port', (process.env.PORT || 3000))
 app.use(expressLayouts)
@@ -13,6 +14,7 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about', { usuario: user })
 })
+
 app.use(express.static(__dirname + '/public/'))
 
 app.listen(app.get('port'), () =>
