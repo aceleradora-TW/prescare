170aaddf6db66817d25629e92ec9d5eda253487f
marcos012 <marcospaulo.ad@gmail.com>
2018-05-15 12:59:58 -0300

    :construction:<@marcos012, @DiovaneMendes> Faz alteracoes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('port', (process.env.PORT || 3000))
 app.get('/table', routes.table)
 
 app.listen(app.get('port'), () =>
-  console.log("Servidor rodando na porta: "+app.get('port')))
+  console.log("Servidor rodando na porta: " + app.get('port')))
 
 //continuar tutorial
 //https://medium.com/@yoobi55/setting-up-heroku-server-with-node-js-express-eng-b0c8cc61a2a9
