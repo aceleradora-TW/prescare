2e8841c4df34da4b4c38bbc51b3426a9b0096bbd
alineabich <aline.abich@acad.pucrs.br>
2018-05-15 10:28:13 -0300

    :rocket: <@alineabich, @claudiastrm> configura ejs

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const user = require('./public/js/user')
 
 app.set('port', (process.env.PORT || 3000))
+app.use(expressLayouts)
+app.set('view engine','ejs')
+
 
 app.get('/', (req, res) => {
-  res.send('Bye Aceleradora!!!')
+  res.render('home')
 })
+app.use(express.static(__dirname + '/public/'))
 
 app.listen(app.get('port'), () =>
   console.log("Servidor rodando na porta: "+app.get('port')))
-
-//continuar tutorial
-//https://medium.com/@yoobi55/setting-up-heroku-server-with-node-js-express-eng-b0c8cc61a2a9
---
+++ public/js/user.js
@@ -0,0 +1,7 @@
+const user = {
+    name: 'Márcia',
+    email: 'marcia@gmail.com',
+    avatar: 'https://www.w3schools.com/css/img_chania.jpg'
+  }
+  
+  module.exports = user
