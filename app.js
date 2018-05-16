f53fafa0c118575bf0d866d50a7b8b49056ce3a4
DiovaneMendes <diovane.m.mendes@gmail.com>
2018-05-16 14:38:34 -0300

    :art: <@ClaudiaStrm> atualiza com a master

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ const ejs = require('ejs')
 const user = require('./public/js/user')
 const prescricao = require('./public/js/prescricao')
 
-app.set('port', (process.env.PORT || 3000))
+app.set('PORT', (process.env.PORT || 3000))
 app.use(expressLayouts)
 app.set('view engine','ejs')
 
 app.get('/', (req, res) => {
-  res.render('tabela', { prescricoes: prescricao })
+  res.render('pages/home', { prescricoes: prescricao })
 })
-app.get('/tabela', (req, res) => {
-    res.render('tabela-prescricao', { prescricoes: prescricao })
-  })
+app.get('/tabela-prescricao', (req, res) => {
+    res.render('pages/tabela-prescricao', { prescricoes: prescricao })
+})
 app.get('/about', (req, res) => {
-  res.render('about', { usuario: user })
+  res.render('pages/about', { usuario: user })
 })
 
 app.use(express.static(__dirname + '/public/'))
 
-app.listen(app.get('port'), () =>
-  console.log("Servidor rodando na porta: " + app.get('port')))
+app.listen(app.get('PORT'), () =>
+  console.log("Servidor rodando na porta: " + app.get('PORT')))
