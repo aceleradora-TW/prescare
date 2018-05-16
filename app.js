62d442b947864f80e46ef803230904562438069c
marcos012 <marcospaulo.ad@gmail.com>
2018-05-16 11:20:29 -0300

    :construction: <@marcos012, @laurascclaro> adiciona dados fakes na tabela

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
-const ejs = require ('ejs')
-
+const ejs = require('ejs')
+const acolhido = require('./public/js/acolhido.js')
 
 
 app.set('view engine', 'ejs')
@@ -12,9 +12,13 @@ app.set('port', (process.env.PORT || 3000))
 app.get('/', (req, res) => {
   res.render('pages/info')
 })
+app.get('/acolhido', (req, res) => {
+  res.render('pages/info', {acolhido:acolhido})
+})
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), () =>
-  console.log("Servidor rodando na porta: "+app.get('port')))
+  console.log("Servidor rodando na porta: " + app.get('port')))
 
 //continuar tutorial
 //https://medium.com/@yoobi55/setting-up-heroku-server-with-node-js-express-eng-b0c8cc61a2a9
