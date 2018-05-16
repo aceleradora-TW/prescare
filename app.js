a345e64e2a8bdc085f6bb329dbf099c65614cc57
luizfetrindade <luiz.fetrindade@gmail.com>
2018-05-16 10:50:31 -0300

    :rocket: <@EngelFlores,@luizfetrindade> Adiciona front farmaceutica

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const app = express()
-const expressLayouts = require ('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts')
 const user = require('./public/js/user')
 const port = 3000
 
 app.set('port', (process.env.port || 3000))
 app.use(expressLayouts)
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
 
 app.get('/', (req, res) => {
-  res.render('home')
+  res.render('pages/home')
 })
 app.get('/about', (req, res) => {
-  res.render('about', { usuario: user })
+  res.render('pages/about', { usuario: user })
 })
-app.get('/farmaceutica',(req,res)=>{
-  res.render('farmaceutica',{usuario: user})
+app.get('/farmaceutica', (req, res) => {
+  res.render('pages/farmaceutica', { usuario: user })
 })
 app.use(express.static(__dirname + '/public/'))
 
