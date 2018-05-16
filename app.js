31446857a56e256899c6e186f25484d780a4f81c
DiovaneMendes <diovane.m.mendes@gmail.com>
2018-05-16 15:48:24 -0300

    :art: <@ClaudiaStrm> refatora variavel port no app.js

--- app.js
+++ app.js
@@ -6,25 +6,21 @@ const ejs = require('ejs')
 const prescricao = require('./public/js/prescricao')
 const user = require('./public/js/user')
 
+const PORT = process.env.PORT || 3000
 
-app.set('PORT', (process.env.PORT || 3000))
 app.use(expressLayouts)
 app.set('view engine','ejs')
 
 app.get('/', (req, res) => {
-  res.render('pages/home', { prescricoes: prescricao })
+  res.render('pages/home')
 })
 app.get('/tabela-prescricao', (req, res) => {
-    res.render('pages/tabela-prescricao', { prescricoes: prescricao })
+  res.render('pages/tabela-prescricao', { prescricoes: prescricao })
 })
-app.get('/tabela', (req, res) => {
-    res.render('tabela-prescricao', { prescricoes: prescricao })
-  })
 app.get('/about', (req, res) => {
   res.render('pages/about', { usuario: user })
 })
 
 app.use(express.static(__dirname + '/public/'))
 
-app.listen(app.get('PORT'), () =>
-  console.log("Servidor rodando na porta: " + app.get('PORT')))
+app.listen(PORT, () => console.log('Rodando na porta ' + PORT))
