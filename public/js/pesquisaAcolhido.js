ee65a047690aa845c9be79236e33bf2c6a9e9348
marcos012 <marcospaulo.ad@gmail.com>
2018-05-17 13:47:16 -0300

    :art: <@lindice, @ilizakoski> filtro para puxar os dados

---
+++ public/js/pesquisaAcolhido.js
@@ -0,0 +1,18 @@
+var prescisao = [
+    { value: 'Pomada', data: 'PO' },
+    { value: 'Lindice', data: 'LI' },
+    { value: 'Ismael', data: 'IS' },
+    { value: 'Remedio', data: 'RE' }
+ ];
+
+ //Puxar os dados em array(acima)
+ $('#campo-pesquisa').autocomplete({
+    lookup: prescisao,
+});
+
+//Puxar os dados em banco de dados
+// $('#campo-pesquisa').autocomplete({
+//     serviceUrl: '/prescicao',
+// });
+
+
