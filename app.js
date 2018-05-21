9f43232dd72c1ee287be9fb4100d9c5894f93145
alineabich <aline.abich@acad.pucrs.br>
2018-05-21 14:03:51 -0300

    :art: <@alineabich, @laurascclaro, @marcos012, @luizfetrindade> Arruma rotas

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const expressLayouts = require('express-ejs-layouts')
 const ejs = require('ejs')
 const user = require('./public/js/user')
 const routes = require('./src/routes/routes')
+
 const PORT = process.env.PORT || 3000
 const DB_NAME = 'prescare'
 const DB_USER = 'postgres'
 const DB_PASSWORD = 'prescare'
 const DB_HOST = 'localhost'
+
+const homeRoute = require('./src/routes/home')
 
 const startApplication = () => {
 
@@ -18,14 +21,12 @@ const startApplication = () => {
     .use(expressLayouts)
     .use(express.static(__dirname + '/public/'))
     .set('view engine', 'ejs')
-    .get('/', (req, res) => {
-      res.render('pages/home')
-    })
+    .get('/', homeRoute)
     .get('/about', (req, res) => {
       res.render('pages/about', { usuario: user })
     })
     
-    .get('/prescricaoAtualizada', routes.prescr)
+    .get('/prescricaoatualizada', routes.prescr)
 
 
     .listen(PORT, () => console.log('Servidor iniciado em http://localhost:' + PORT))
