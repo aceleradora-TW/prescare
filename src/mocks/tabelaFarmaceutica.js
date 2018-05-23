fa1e30a149dd2408f2ab8054ba8958fdb53d4b24
ClaudiaStrm <s_moura@hotmail.com>
2018-05-23 11:31:37 -0300

    :art: <@alineabich, @ClaudiaStrm> ajusta prescricaoAtualizada.ejs

--- src/mocks/tabelaFarmaceutica.js
+++ src/mocks/tabelaFarmaceutica.js
@@ -1,7 +1,7 @@
 const tabelaFarmaceutica = [
 {
-    name: 'Afonso',
-    farm: [{
+    nome: 'Afonso',
+    farmaceutica: [{
         medicamentos: 'Paracetamol 500mg',
         via: 'Oral',
         intervalo: '8-8h',
@@ -13,8 +13,8 @@ const tabelaFarmaceutica = [
     }]
 },
 {
-    name: 'Olimar',
-    farm: [{
+    nome: 'Olimar',
+    farmaceutica: [{
         medicamentos: 'Busonide 100mg',
         via: 'Oral',
         intervalo: '16-16h',
@@ -26,9 +26,19 @@ const tabelaFarmaceutica = [
     }] 
 },
 {
-    name: 'Alvaro',
-    farm: [{
+    nome: 'Alvaro',
+    farmaceutica: [{
         medicamentos: 'Ritalina 25mg',
+        via: 'Oral',
+        intervalo: '24-24h',
+        formaFarmaceutica: 'gotas',
+        horario:'8h 16h',
+        lote: '1234',
+        validade: '03/12/2019',
+        dispensacao: false,
+    },
+    {
+        medicamentos: 'Paracetamol 25mg',
         via: 'Oral',
         intervalo: '24-24h',
         formaFarmaceutica: 'gotas',
@@ -39,8 +49,8 @@ const tabelaFarmaceutica = [
     }] 
 },
 {
-    name: 'Gilberto',
-    farm: [{
+    nome: 'Gilberto',
+    farmaceutica: [{
         medicamentos: 'Clonazepam 2.5mg',
         via: 'Oral',
         intervalo: '24-24h',
