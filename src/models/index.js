3a60daa4894f3a9469693d607605476ab0ca44a9
EngelFlores <engel.tads@gmail.com>
2018-06-04 13:29:40 -0300

    :rocket: <@EngelFlores,@lindicell> Cria models

--- src/models/index.js
+++ src/models/index.js
@@ -17,12 +17,31 @@ module.exports = db => {
   Prescricao.belongsTo(Acolhido)
   Prescricao.hasMany(Medicamento)
   Acolhido.hasMany(Prescricao)
+<<<<<<< 6c8c9ea9c54affd40c7c5c7a9a1888abf5984be3
   Medicamento.belongsTo(Prescricao)
   Cuidado.belongsTo(Prescricao)
   Dieta.belongsTo(Prescricao)
   Prescricao.belongsToMany(Usuario,{through:'usuario_prescricao'})
   Prescricao.hasMany(Cuidado)
   Prescricao.hasMany(Dieta)
+=======
+  Medicamento.belongsToMany(Prescricao, {through: 'prescricao_medicamento'})
+  Cuidado.belongsToMany(Prescricao)
+  Dieta.belongsToMany(Prescricao)
+  Prescricao.belongsToMany(Usuario)
+>>>>>>> :rocket: <@EngelFlores,@lindicell> Cria models
 
   return {Acolhido, Medicamento, Prescricao, Cuidado, Dieta}
 }
+
+// exibir prescricao com os medicamentos 
+// como introduzir ele no projeto
+// como ele funciona por de trás dos panos 
+// comandos 
+// End to End (baby steps)
+// pq usar 
+// exemplo no projeto 
+// quisso
+// ORM 
+// CRUD 
+// QUERY 
