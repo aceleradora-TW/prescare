309c8b69a13023dcf8b03619a67defc2b035934d
murilobauerc <panchobauer@gmail.com>
2018-06-04 16:07:53 -0300

    :construction: <@murilobauerc, @ilizakoski> renderiza prescricoes de acolhido

---
+++ src/routes/acolhido.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize')
+
+const acolhido = (Acolhido, Prescricao) => (req, res) => {
+    return Acolhido.find({
+        where: {
+            id: req.params.id
+        },
+        include: [Prescricao]
+    }).then(acolhido => {
+        res.render('pages/info', { acolhido: acolhido, presc: acolhido.prescricaos })
+    })
+}
+
+module.exports = acolhido
