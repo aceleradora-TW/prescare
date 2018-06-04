93a4e63afbb1ed0d3a7ab8cdba3b656f9a97753c
diovanemendes <diovane.m.mendes@gmail.com>
2018-06-04 16:05:31 -0300

    :rocket: <@natymoraes , @diovanemendes> Cria botão salvar e voltar com link #23

--- app.js
+++ app.js
@@ -34,7 +34,42 @@ const startApplication = () => {
     .use('/', routes.applicationRoutes)
     .use('/acolhido', routes.acolhidoRoutes)
     .use('/acolhido/:acolhido_id/prescricao', routes.prescricaoRoutes)
+<<<<<<< 262c4491b7590eaf8e244a04e2b5a10ca05903c6
 
+<<<<<<< 73a2a91d7ca9e4d81cfcbe7da0a9bdb512513694
+=======
+<<<<<<< c2998168d9853e6436e0d63e75b3b966cbf2160b
+<<<<<<< 9546b7f5e4d175f09346d462f5d8673f4d969912
+=======
+    .get('/login', (req, res) => {
+      res.render('pages/login');
+    })
+    .get('/pesquisar', (req, res) => {
+      res.render('pages/pesquisa-acolhidos');
+    })
+    .get('/historico', (req, res) => {
+      res.render('pages/historico-prescricao');
+    })
+    .get('/', routes.home)
+    .get('/about', routes.about)
+    .get('/lista-acolhidos', routes.listaAcolhidos)
+    .get('/prescricao-atualizada', routes.prescricaoAtualizada)
+    .get('/farmaceutica', routes.farmaceutica)
+
+    .get('/acolhido/:acolhido_id', routes.acolhido)
+    .post('/acolhido/:acolhido_id/prescricao', routes.createPrescricao)
+    .get('/acolhido/:acolhido_id/prescricao/:prescricao_id', routes.getPrescricao)
+    .get('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit', routes.editPrescricao)
+    .post('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit', routes.updatePrescricao)
+    .delete('/acolhido/:acolhido_id/prescricao/:prescricao_id', routes.destroyPrescricao)
+=======
+>>>>>>> Removendo roteamento do app e utilizando routes index para isso
+=======
+    .use('/', routes.cuidadoRoutes)
+>>>>>>> :rocket: <@natymoraes , @diovanemendes> Cria botão salvar e voltar com link #23
+    
+>>>>>>> Fazendo o CRUD de prescrição para acolhido, baseado na discussão do dia 01/06/2018
+>>>>>>> :rocket: <@natymoraes , @diovanemendes> Cria botão salvar e voltar com link #23
     .listen(settings.PORT, () =>
       console.log('Servidor iniciado em http://localhost:' + settings.PORT)
     );
