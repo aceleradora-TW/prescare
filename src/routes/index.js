6c8c9ea9c54affd40c7c5c7a9a1888abf5984be3
ghidorsi <ghidorsi@thoughtworks.com>
2018-06-04 12:09:34 -0300

    Removendo roteamento do app e utilizando routes index para isso

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,38 +1,11 @@
-const listaAcolhidos = require('./acolhido/list')
-const home = require('./application/home')
-const about = require('./application/about')
-const acolhido = require('../routes/acolhido/get')
-const farmaceutica = require('../routes/farmaceutica')
-const prescricaoAtualizada = require('../routes/prescricaoAtualizada')
-
-const user = require('../mocks/user')
-const tabelaFarmaceutica = require('../mocks/tabelaFarmaceutica')
-const dadosFarmacia = require('../mocks/farmacia')
-
-const createPrescricao = require('./prescricao/create')
-const editPrescricao = require('./prescricao/edit')
-const getPrescricao = require('./prescricao/get')
-const destroyPrescricao = require('./prescricao/destroy')
-const updatePrescricao = require('./prescricao/update')
+const prescricaoRoutes = require('./prescricao')
+const acolhidoRoutes = require('./acolhido')
+const applicationRoutes = require('./application')
 
 const allRoutes = models => ({
-  listaAcolhidos: listaAcolhidos(models.Acolhido),
-  home: home(),
-  about: about(user),
-  acolhido: acolhido(models.Acolhido),
-  prescricaoAtualizada: prescricaoAtualizada(tabelaFarmaceutica),
-  farmaceutica: farmaceutica(dadosFarmacia),
-
-  createPrescricao: createPrescricao(models.Prescricao),
-  editPrescricao: editPrescricao(models.Prescricao),
-  getPrescricao: getPrescricao(models.Prescricao),
-  destroyPrescricao: destroyPrescricao(models.Prescricao),
-  updatePrescricao: updatePrescricao(models.Prescricao),
-
-  createCuidado: createCuidado(models.Cuidado),
-  editCuidado: editCuidado(models.Cuidado),
-  getCuidado: getCuidado(models.Cuidado),
-  updateCuidado: updateCuidado(models.Cuidado)
+  acolhidoRoutes: acolhidoRoutes(models.Acolhido),
+  prescricaoRoutes: prescricaoRoutes(models.Prescricao),
+  applicationRoutes: applicationRoutes()
 })
 
 module.exports = allRoutes
