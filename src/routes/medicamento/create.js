93dd3893ff132d475554b265d6d27ca651dfa7cd
marcos012 <marcospaulo.ad@gmail.com>
2018-06-05 13:38:06 -0300

    :rocket: <@marcos012, @ClaudiaStrm> Adiciona medicamentos no banco

---
+++ src/routes/medicamento/create.js
@@ -0,0 +1,7 @@
+module.exports = Medicamento => (req, res) => {
+    Medicamento.create({
+        prescricao_id: req.params.prescricao_id
+    }).then(medicamento => {
+        res.redirect(req.originalUrl + '/' + medicamento.id + '/edit')
+    })
+}
---
+++ src/routes/medicamento/edit.js
@@ -0,0 +1,12 @@
+module.exports = Medicamento => (req, res) => {
+    Medicamento.findOne({
+      where: {
+        id: req.params.medicamento_id
+      }
+    }).then(medicamento => {
+      if(!medicamento) res.render('/404')
+  
+      res.render('pages/editarMedicamento', { medicamento, updateUrl: req.originalUrl})
+    })
+  }
+  
