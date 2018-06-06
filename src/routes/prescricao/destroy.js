c2885cf8369aaf93c4d49a6b47c45d3010e54aa2
ilizakoski <ilizakoski@gmail.com>
2018-06-06 15:17:33 -0300

    :rocket: <@ilizakoski, @murilobauerc> Excluir a prescrição

--- src/routes/prescricao/destroy.js
+++ src/routes/prescricao/destroy.js
@@ -3,5 +3,11 @@ module.exports = Prescricao => (req, res) => {
     where: {
       id: req.params.prescricao_id
     }
+<<<<<<< 5c9392b050fdc52720a9e13aff86ccb692f52b88
   }).then(() => { console.log('Prescrição deletada.') })
+=======
+  }).then(() => { 
+    res.render('pages/editarPrescricao', { prescricao, updateUrl: req.originalUrl })
+    })
+>>>>>>> :rocket: <@ilizakoski, @murilobauerc> Excluir a prescrição
 }
