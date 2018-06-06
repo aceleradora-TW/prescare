d131ab5cac3cea0a101b15a2432e5d19c37eebf1
laurascclaro <laurascclaro@hotmail.com>
2018-06-06 15:14:42 -0300

    :rocket: <@laurascclaro, @marcos012> adiciona botao voltar

--- src/routes/dieta/edit.js
+++ src/routes/dieta/edit.js
@@ -1,4 +1,6 @@
 module.exports = Dieta => (req, res) => {
+  console.log(req.params);
+  
     Dieta.findOne({
       where: {
         id: req.params.dieta_id
@@ -6,7 +8,12 @@ module.exports = Dieta => (req, res) => {
     }).then(dieta => {
       if(!dieta) res.render('/404')
   
-      res.render('pages/editarDieta', { dieta, updateUrl: req.originalUrl})
+      res.render('pages/editarDieta', { 
+        acolhidoId: req.params.acolhido_id,
+        prescricaoId: req.params.prescricao_id,
+        dieta, 
+        updateUrl: req.originalUrl
+      })
     })
   }
   
