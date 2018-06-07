54f5840a14acc47a2a573b104283b1210d00a468
diovanemendes <diovane.m.mendes@gmail.com>
2018-06-07 13:19:53 -0300

    :bug: <@ClaudiaStrm, @DiovaneMendes> arruma rotas de cuidado #23

--- src/routes/prescricao/index.js
+++ src/routes/prescricao/index.js
@@ -1,5 +1,3 @@
-const router = require('express').Router()
-
 const create = require('./create')
 const edit = require('./edit')
 const get = require('./get')
@@ -12,6 +10,6 @@ module.exports = (Prescricao, Dieta, router) => {
   router.get('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit', edit(Prescricao, Dieta))
   router.post('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit', update(Prescricao))
   router.delete('/acolhido/:acolhido_id/prescricao/:prescricao_id/destroy', destroy(Prescricao))
-  
+
   return router;
 }
