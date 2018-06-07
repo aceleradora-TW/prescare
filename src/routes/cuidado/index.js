cca22f9d6484263a21b931507b798b45090ebd02
diovanemendes <diovane.m.mendes@gmail.com>
2018-06-07 09:59:15 -0300

    :bug: <@Claudiastrm, @DiovaneMendes> Arruma rota create #23

--- src/routes/cuidado/index.js
+++ src/routes/cuidado/index.js
@@ -5,11 +5,10 @@ const edit = require('./edit')
 const update = require('./update')
 const destroy = require('./destroy')
 
-module.exports = (Cuidado, router) => {
-  router.post('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/cuidado', create(Cuidado))
-  router.get('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/cuidado/:cuidado_id/', get(Cuidado))
-  router.get('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/cuidado/:cuidado_id/edit', edit(Cuidado))
-  router.post('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/cuidado/:cuidado_id/edit', update(Cuidado))
-  router.post('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/cuidado/:cuidado_id/edit/excluir', destroy(Cuidado))
+module.exports = Cuidado => {
+  router.post('/', create(Cuidado))
+  router.get('/:cuidado_id', get(Cuidado))
+  router.get('/:cuidado_id/edit', edit(Cuidado))
+  router.post('/:cuidado_id/edit', update(Cuidado))
   return router;
 }
