2e8ac3cb540e30ed45c46d31695d7b3640d2e58a
EngelFlores <engel.tads@gmail.com>
2018-06-11 15:06:04 -0300

    :rocket: <@murilobauerc, @alineabich, @ilizakoski> implementa fluxo e adiciona validade prescricao

--- src/routes/medicamento/index.js
+++ src/routes/medicamento/index.js
@@ -1,5 +1,14 @@
+const create = require('./create')
 const get = require('./get')
+const edit = require('./edit')
+const update = require('./update')
+const destroy = require('./destroy')
 
-module.exports = Medicamento => ({
-    get: get(Medicamento),
-  })
+module.exports = (Medicamento, router) => {
+  router.post('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/medicamento', create(Medicamento))
+  router.get('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/medicamento/:medicamento_id/', get(Medicamento))
+  router.get('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/medicamento/:medicamento_id/edit', edit(Medicamento))
+  router.post('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/medicamento/:medicamento_id/edit', update(Medicamento))
+  router.post('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/medicamento/:medicamento_id/edit/excluir', destroy(Medicamento))
+  return router;
+}
