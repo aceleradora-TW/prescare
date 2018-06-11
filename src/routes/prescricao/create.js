c5a81ee715bd02cbe302a7b7c021efb2441d4289
laurascclaro <laurascclaro@hotmail.com>
2018-06-11 14:39:41 -0300

    :bug: <@camirmarques, @laurascclaro> arruma bug depois de atualizar com a master

--- src/routes/prescricao/create.js
+++ src/routes/prescricao/create.js
@@ -11,13 +11,9 @@
 // }
 
 module.exports = Prescricao => (req, res) => {
-<<<<<<< 4e14796c2d5c14d09743571f1618d6768233f2cb
   return Prescricao
   .create({
     acolhido_id: req.params.acolhido_id
-=======
-  Prescricao.find({
->>>>>>> :bug: <@EngelFlores,@alineabich> Criar nova prescricao #25
   }).then(prescricao => {
     if (!prescricao){
     res.redirect(req.originalUrl + '/' + prescricao.id + '/edit')
