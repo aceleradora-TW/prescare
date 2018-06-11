c0df2163eea782f6f4d087646033ecd9922bc674
laurascclaro <laurascclaro@hotmail.com>
2018-06-11 14:39:41 -0300

    :bug: <@camirmarques, @laurascclaro> arruma bug depois de atualizar com a master

--- src/routes/prescricao/create.js
+++ src/routes/prescricao/create.js
@@ -1,3 +1,15 @@
+// module.exports = Prescricao => (req, res) => {
+//   Prescricao.find({
+//   }).then(prescricao => {
+//     if (!prescricao){
+//     res.redirect(req.originalUrl + '/' + prescricao.id + '/edit')
+//   }
+//   else{
+//     res.render('/:prescricao_id')
+//   }
+//   })
+// }
+
 module.exports = Prescricao => (req, res) => {
   return Prescricao
   .create({
