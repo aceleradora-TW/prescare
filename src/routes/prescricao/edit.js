5e2745e0db791392e569efad6dbe9a1fdbb1fce9
camirmarques <cam.romarques@gmail.com>
2018-06-12 15:18:01 -0300

    <@luizfetrindade, camirmarques> :fire: Adiciona rota prescricao cuidado

--- src/routes/prescricao/edit.js
+++ src/routes/prescricao/edit.js
@@ -1,20 +1,20 @@
 module.exports = (Prescricao, Cuidado, Dieta, Acolhido) => (req, res) => {
-    return Prescricao.findOne({
+  return Prescricao.findOne({
     where: {
       id: req.params.prescricao_id
     },
-    include:[Cuidado, Dieta, Acolhido]
-      })
-  .then(prescricao => {
-    if(!prescricao) res.render('/404')
-    
-    res.render('pages/editarPrescricao', { 
-      prescricao, 
-      acolhido: prescricao.acolhido,
-      cuidado : prescricao.cuidados,
-      dietas: prescricao.dieta,
-      cuidado : prescricao.cuidados,
-      updateUrl: req.originalUrl,
-     })
+    include: [Cuidado, Dieta, Acolhido]
   })
+    .then(prescricao => {
+      if (!prescricao) res.render('/404')
+
+      res.render('pages/editarPrescricao', {
+        prescricao,
+        acolhido: prescricao.acolhido,
+        cuidado: prescricao.cuidados,
+        dietas: prescricao.dieta,
+        cuidado: prescricao.cuidados,
+        updateUrl: req.originalUrl,
+      })
+    })
 }
