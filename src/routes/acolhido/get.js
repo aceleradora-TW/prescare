7234b027c88de4e04386507f103aede205df51fd
alineabich <aline.abich@acad.pucrs.br>
2018-06-12 15:58:31 -0300

    :art: <@murilobauerc, @alineabich, @ilizakoski> arruma css e corrigindo coisas

--- src/routes/acolhido/get.js
+++ src/routes/acolhido/get.js
@@ -6,11 +6,7 @@ module.exports = (Acolhido, Prescricao) => (req, res) => {
       },
       include: [Prescricao]
     }).then(acolhido => {
-<<<<<<< 8b9870ae64115aeed313a85a7e4c876ffe4de339
       res.render('pages/acolhido', { acolhido, prescricoes: acolhido.prescricaos})
-=======
-      res.render('pages/acolhido', { acolhido, presc: acolhido.prescricaos})
->>>>>>> :rocket: <@murilobauerc, @alineabich, @DiovaneMendes> implementa fluxo
     })
     .catch(console.log)
 }
