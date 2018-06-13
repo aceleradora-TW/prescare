dc8e7b46fd78e5979a4266ce2466b0a3b58e3baf
ClaudiaStrm <s_moura@hotmail.com>
2018-06-13 10:04:01 -0300

    :fire: <@Claudiastrm> exclui arquivos desnecessários

--- src/routes/acolhido/listaAcolhidos.js
+++ src/routes/acolhido/listaAcolhidos.js
@@ -2,7 +2,7 @@ module.exports = Acolhido => (req, res) => {
   return Acolhido
     .findAll()
     .then(acolhidos => {
-      acolhidos.sort(function(a,b) {
+      acolhidos.sort(function(a, b) {
         if (a.nome > b.nome) return 1
         if (b.nome > a.nome) return -1
         return 0
