f165eddd67db66bb28e9e39d84059cd67e22557a
luizfetrindade <luiz.fetrindade@gmail.com>
2018-06-13 13:31:59 -0300

    <@luizfetrindade, @ilizakoski> :rocket: Adiciona Test Dieta

--- src/routes/dieta/create.js
+++ src/routes/dieta/create.js
@@ -1,5 +1,5 @@
 module.exports = Dieta => (req, res) => {
-    Dieta.create({
+    return Dieta.create({
         prescricao_id: req.params.prescricao_id
     }).then(dieta => {
         res.redirect(req.originalUrl + '/' + dieta.id + '/edit')
