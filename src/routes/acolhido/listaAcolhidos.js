87030ce0c7ee7167537c9e00c5056873f97f6171
luizfetrindade <luiz.fetrindade@gmail.com>
2018-06-13 09:57:06 -0300

    :rocket: <@Claudiastrm> retorna lista ordenada por nome #14

--- src/routes/acolhido/listaAcolhidos.js
+++ src/routes/acolhido/listaAcolhidos.js
@@ -2,6 +2,11 @@ module.exports = Acolhido => (req, res) => {
   return Acolhido
     .findAll()
     .then(acolhidos => {
+      acolhidos.sort(function(a,b) {
+        if (a.nome > b.nome) return 1
+        if (b.nome > a.nome) return -1
+        return 0
+      })
       res.render('pages/listaAcolhidos', { acolhidos })
     })
     .catch(console.log) 
