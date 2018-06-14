7015cf24e2b2726c0c3886417f4bac1712afaa7f
EngelFlores <engel.tads@gmail.com>
2018-06-14 14:17:07 -0300

    :construction: <@laurascclaro, @EngelFlores>  rebasing

--- src/routes/acolhido/index.js
+++ src/routes/acolhido/index.js
@@ -1,15 +1,16 @@
+
 const router = require('express').Router()
 
 const edit = require('./edit')
 const get = require('./get')
 const update = require('./update')
-const list = require('./list')
+const list = require('./listaAcolhidos')
 
-module.exports = (Acolhido, Prescricao, router) => {
-  router.get('/acolhido/lista', list(Acolhido))
-  router.get('/acolhido/:acolhido_id', get(Acolhido, Prescricao))
+module.exports = (Acolhido, router) => {
+  router.get('/lista-acolhidos', list(Acolhido))
+  router.get('/acolhido/:acolhido_id', get(Acolhido))
   router.get('/acolhido/:acolhido_id/edit', edit(Acolhido))
   router.post('/acolhido/:acolhido_id/edit', update(Acolhido))
+
   return router;
 }
-
