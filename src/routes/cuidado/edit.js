34df4b8d2959a0e10851315779d17c367117845d
ClaudiaStrm <s_moura@hotmail.com>
2018-06-14 16:03:45 -0300

    :rocket: <@ClaudiaStrm, @marcos012> Adiciona cabeçalho de acolhido nas partials

--- src/routes/cuidado/edit.js
+++ src/routes/cuidado/edit.js
@@ -4,12 +4,15 @@ module.exports = (Cuidado, Prescricao, Acolhido) => (req, res) => {
             where: {
                 id: req.params.cuidado_id
             },
-            include: [
-                {
-                    model: Prescricao, where: { id: req.params.prescricao_id },
-                    include: [{ model: Acolhido, where: { id: req.params.acolhido_id } }]
-                }
-            ]
+            include: [{
+                model: Prescricao, where: {
+                    id: req.params.prescricao_id
+                }, include: [{
+                    model: Acolhido, where: {
+                        id: req.params.acolhido_id
+                    }
+                }]
+            }]
         }).then(cuidado => {
             res.render('pages/editarCuidado', {
                 acolhidoId: req.params.acolhido_id,
