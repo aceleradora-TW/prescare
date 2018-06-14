daea9406ed039907613e98be8128f185e78f478d
natymoraes <naty.moraes16@gmail.com>
2018-06-14 14:44:30 -0300

    :art: <@natymoraes @luizfetrindade> Refatora Rotas

--- src/routes/cuidado/destroy.js
+++ src/routes/cuidado/destroy.js
@@ -4,7 +4,6 @@ module.exports = Cuidado => (req, res) => {
             id: req.params.cuidado_id
         }
     }).then(() => {
-        res.render('/acolhido/' + req.params.acolhido_id + '/prescricao/' + req.params.prescricao_id + '/edit')
-        // res.redirect('/acolhido/' + req.params.acolhido_id + '/prescricao/' + req.params.prescricao_id + '/edit')
+        res.redirect('/acolhido/' + req.params.acolhido_id + '/prescricao/' + req.params.prescricao_id + '/edit')
     })
 }
