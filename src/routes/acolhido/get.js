b22501cbb7658c2bca78cd35955d73d6755e8c96
murilobauerc <panchobauer@gmail.com>
2018-06-14 14:39:50 -0300

    :construction: <@murilobauerc, @ilizakoski> criando rotas e campos farmaceutica

--- src/routes/acolhido/get.js
+++ src/routes/acolhido/get.js
@@ -1,9 +1,10 @@
-module.exports = Acolhido => (req, res) => {
+module.exports = (Acolhido, Prescricao) => (req, res) => {
   return Acolhido.findOne({
     where: {
       id: req.params.acolhido_id
-    }
+    },
+    include: [Prescricao]
   }).then(acolhido => {
-    res.render('pages/infoAcolhido', { acolhido: acolhido})
+    res.render('pages/infoAcolhido', { acolhido: acolhido, prescricaos: acolhido.prescricao })
   }).catch(err => console.log(err))  
 }
