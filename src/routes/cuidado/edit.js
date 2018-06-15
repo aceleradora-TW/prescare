70303201a82a00bfcbc82a849986ff27a6bef46b
alineabich <aline.abich@acad.pucrs.br>
2018-06-15 13:03:17 -0300

    :art:<@alineabich, @marcos012> Atualiza com a master

--- src/routes/cuidado/edit.js
+++ src/routes/cuidado/edit.js
@@ -11,8 +11,8 @@ module.exports = (Cuidado, Prescricao, Acolhido) => (req, res) => {
         }).then(cuidado => {
             if (!cuidado) res.send('Essa página não existe')
             res.render('pages/editarCuidado', {
-                acolhidoId: req.params.acolhido_id,
-                prescricaoId: req.params.prescricao_id,
+                acolhido_id: req.params.acolhido_id,
+                prescricao_id: req.params.prescricao_id,
                 cuidado,
                 updateUrl: req.originalUrl,
                 acolhido: cuidado.prescricao.acolhido
