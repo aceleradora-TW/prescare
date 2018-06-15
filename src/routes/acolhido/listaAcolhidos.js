aa274e355ee17e599fb0be2a9c74ae4abbe47978
murilobauerc <panchobauer@gmail.com>
2018-06-15 13:52:56 -0300

    :art: <@murilobauerc, @ClaudiaStrm> refatora ordenacao acolhidos

--- src/routes/acolhido/listaAcolhidos.js
+++ src/routes/acolhido/listaAcolhidos.js
@@ -1,12 +1,9 @@
 module.exports = Acolhido => (req, res) => {
   return Acolhido
-    .findAll()
+    .findAll({
+      order: ['nome']
+    })
     .then(acolhidos => {
-      acolhidos.sort(function(a, b) {
-        if (a.nome > b.nome) return 1
-        if (b.nome > a.nome) return -1
-        return 0
-      })
       res.render('pages/listaAcolhidos', { acolhidos })
     })
     .catch(console.log) 
