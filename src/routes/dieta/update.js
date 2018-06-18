919eb8359125aa45c1c9c00694cf09c0d4796874
diovanemendes <diovane.m.mendes@gmail.com>
2018-06-18 09:50:28 -0300

    :bug: <@lindicell, @diovanemendes> Conserta teste update #24

--- src/routes/dieta/update.js
+++ src/routes/dieta/update.js
@@ -5,8 +5,9 @@ module.exports = Dieta => (req, res) => {
       id: req.params.dieta_id
     }
   }).then(dieta => {
-    if (!dieta) return res.send('Essa página não existe.')
-    dieta.update(req.body).then(() => {
+    if(!dieta) res.send('Essa página não existe')
+    dieta.update(req.body)
+    .then(() => {
       res.redirect('/acolhido/' + req.params.acolhido_id + '/prescricao/' + req.params.prescricao_id + '/edit')
     })
   })
