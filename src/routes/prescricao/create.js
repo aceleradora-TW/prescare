daed344f671e45b7c700bcb5bde63044ded98077
murilobauerc <panchobauer@gmail.com>
2018-06-19 10:48:45 -0300

    :art: <@murilobauerc, @EngelFlores> atualiza com a master e implementa medicamento

--- src/routes/prescricao/create.js
+++ src/routes/prescricao/create.js
@@ -1,25 +1,8 @@
-// module.exports = Prescricao => (req, res) => {
-//   Prescricao.find({
-//   }).then(prescricao => {
-//     if (!prescricao){
-//     res.redirect(req.originalUrl + '/' + prescricao.id + '/edit')
-//   }
-//   else{
-//     res.render('/:prescricao_id')
-//   }
-//   })
-// }
-
 module.exports = Prescricao => (req, res) => {
   return Prescricao
   .create({
     acolhido_id: req.params.acolhido_id
   }).then(prescricao => {
-    if (!prescricao){
     res.redirect(req.originalUrl + '/' + prescricao.id + '/edit')
-  }
-  else{
-    res.render('/:prescricao_id')
-  }
   })
 }
