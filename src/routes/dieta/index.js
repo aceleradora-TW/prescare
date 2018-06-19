0237ea1bb6858c4dc90a4fd3bb5f9bf0f9193705
alineabich <aline.abich@acad.pucrs.br>
2018-06-19 11:21:52 -0300

    :art:<@alineabich, @DiovaneMendes>Refatora

--- src/routes/dieta/index.js
+++ src/routes/dieta/index.js
@@ -5,10 +5,10 @@ const update = require('./update')
 const destroy = require('./destroy')
 
 module.exports = (Dieta, Prescricao, Acolhido, router) => {
-    router.post('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/dieta', create(Dieta))
-    router.get('/acolhido/:acolhido_id/prescricao/:prescricao_id/dieta/:dieta_id', get(Dieta))
-    router.get('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/dieta/:dieta_id/edit', edit(Dieta, Prescricao, Acolhido))
-    router.post('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/dieta/:dieta_id/edit', update(Dieta))
-    router.post('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/dieta/:dieta_id/destroy', destroy(Dieta))
+  router.post('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/dieta', create(Dieta))
+  router.get('/acolhido/:acolhido_id/prescricao/:prescricao_id/dieta/:dieta_id', get(Dieta))
+  router.get('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/dieta/:dieta_id/edit', edit(Dieta, Prescricao, Acolhido))
+  router.post('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/dieta/:dieta_id/edit', update(Dieta))
+  router.post('/acolhido/:acolhido_id/prescricao/:prescricao_id/edit/dieta/:dieta_id/destroy', destroy(Dieta))
   return router;
 }
