92b07d64faab0ce3d57c0ea368ba703e947c49bf
alineabich <aline.abich@acad.pucrs.br>
2018-06-19 10:10:27 -0300

    :rocket:<@alineabich, @DiovaneMendes>Cria botao cancelar e test destroy

---
+++ src/routes/dieta/destroy.js
@@ -0,0 +1,9 @@
+module.exports = Dieta => (req, res) => {
+    return Dieta.destroy({
+        where: {
+            id: req.params.dieta_id
+        }
+    }).then(() => {
+        res.redirect('/acolhido/' + req.params.acolhido_id + '/prescricao/' + req.params.prescricao_id + '/edit')
+    })
+}
---
+++ test/routes/dietasTest/deleteDieta.test.js
@@ -0,0 +1,17 @@
+const dietaRoute = require('../../../src/routes/dieta/destroy')
+
+describe('Quando deleto dieta', () => {
+    it('Deve deletar informações da dieta', (done) => {
+        const Dieta = {
+            destroy: jest.fn().mockResolvedValue()
+        }
+        const req = { params: { prescricao_id: 1, acolhido_id: 2, dieta_id: 3 } }
+        const res = { redirect: jest.fn() }               
+        
+        dietaRoute(Dieta)(req, res)
+        .then(() => expect(Dieta.destroy).toBeCalledWith({"where" :{ "id": req.params.dieta_id }} ))
+        .then(() => expect(res.redirect).toBeCalledWith('/acolhido/2/prescricao/1/edit'))
+        .then(done)
+        .catch(done)
+    })
+})
