be53fed5a61691812389c89e2316e50713757dcc
murilobauerc <panchobauer@gmail.com>
2018-06-19 14:51:35 -0300

    :art: <@EngelFlores,@murilobauerc> Atualiza com a master

--- src/routes/cuidado/edit.js
+++ src/routes/cuidado/edit.js
@@ -4,12 +4,16 @@ module.exports = (Cuidado, Prescricao, Acolhido) => (req, res) => {
             where: {
                 id: req.params.cuidado_id
             },
-            include: [Prescricao,Acolhido]
+            include: [
+                {
+                    model: Prescricao, where: { id: req.params.prescricao_id },
+                    include: [{ model: Acolhido, where: { id: req.params.acolhido_id } }]
+                }
+            ]
         }).then(cuidado => {
-            if (!cuidado) res.send('Essa página não existe')
             res.render('pages/editarCuidado', {
-                acolhido_id: req.params.acolhido_id,
-                prescricao_id: req.params.prescricao_id,
+                acolhidoId: req.params.acolhido_id,
+                prescricaoId: req.params.prescricao_id,
                 cuidado,
                 updateUrl: req.originalUrl,
                 acolhido: cuidado.prescricao.acolhido
