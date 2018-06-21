f1728acdcd5ae32fda59c43ed40547e56e9b5f5b
marcos012 <marcospaulo.ad@gmail.com>
2018-06-21 16:10:27 -0300

    :rocket: <@marcos012, @lindicell> Troca data para formato pt-br (com datepicker)

--- src/routes/prescricao/update.js
+++ src/routes/prescricao/update.js
@@ -1,4 +1,11 @@
+const moment = require('moment')
+
+const FORMATO_DATA = 'DD/MM/YYYY'
+
 module.exports = Prescricao => (req, res) => {
+
+  const dataValidade = moment(req.body.validade, FORMATO_DATA)
+  
   Prescricao.findOne({
     where: {
       id: req.params.prescricao_id
@@ -6,9 +13,14 @@ module.exports = Prescricao => (req, res) => {
   }).then(prescricao => {
     prescricao.update({
       data: new Date().getTime(),
-      validade: req.body.validade
-    }).then(() => {
+      validade: dataValidade.toDate()
+    })
+    .then(() => {
       res.redirect(req.originalUrl)
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.send("Morreu")
     })
   })
 }
