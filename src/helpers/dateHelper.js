9aa7682553c3fa431e90acef5cf77e9d26eac9ce
murilobauerc <panchobauer@gmail.com>
2018-06-25 10:33:28 -0300

    :art: <@murilobauerc, @NatyMoraes> refatora data

--- src/helpers/dateHelper.js
+++
@@ -1,5 +0,0 @@
-
-ajustaData = () => {
-  let $ = document.querySelector.bind(document)
-  `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
-}
--- src/routes/prescricao/update.js
+++ src/routes/prescricao/update.js
@@ -3,24 +3,22 @@ const moment = require('moment')
 const FORMATO_DATA = 'DD/MM/YYYY'
 
 module.exports = Prescricao => (req, res) => {
-
-  const dataValidade = moment(req.body.validade, FORMATO_DATA)
-  
-  Prescricao.findOne({
+  return Prescricao.findOne({
     where: {
       id: req.params.prescricao_id
     },
-  }).then(prescricao => {
-    prescricao.update({
-      data: new Date().getTime(),
-      validade: dataValidade.toDate()
-    })
-    .then(() => {
-      res.redirect(req.originalUrl)
-    })
-    .catch((error) => {
-      console.log(error.message);
-      res.send("Morreu")
-    })
   })
+    .then(prescricao => {
+      prescricao.update({
+        data: new Date().getTime(),
+        validade: moment(req.body.validade, FORMATO_DATA).toDate()
+      })
+        .then(() => {
+          res.redirect(req.originalUrl)
+        })
+        .catch((error) => {
+          console.log(error.message);
+          res.send("Morreu")
+        })
+    })
 }
