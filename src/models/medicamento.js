80358fed2908cb01d5bd4b859c97a2f74277f5f5
EngelFlores <engel.tads@gmail.com>
2018-06-25 14:26:28 -0300

    :art: <@EngelFlores,@camirmarques> Refatora medicamento conforme showcase

--- src/models/medicamento.js
+++ src/models/medicamento.js
@@ -1,14 +1,11 @@
-
 const sequelize = require('sequelize')
 
 module.exports = db => db.define('medicamento', {
   nome: sequelize.STRING,
-  intervalo: sequelize.STRING,
-  via: sequelize.STRING,
-  formaFarmaceutica: sequelize.STRING,
   dosagem:sequelize.STRING,
-  validade:sequelize.STRING,
-  lote:sequelize.STRING,
-  tipo: sequelize.STRING,
-  observacoes: sequelize.STRING
+  formaFarmaceutica: sequelize.STRING,
+  via: sequelize.STRING,
+  posologia: sequelize.STRING,
+  observacoes: sequelize.STRING,
+  validadeMed: sequelize.STRING,
 })
