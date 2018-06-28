7a138612ad32a376dc6eeb22aec9ce320fb86aaf
natymoraes <naty.moraes16@gmail.com>
2018-06-28 15:23:12 -0300

    :bug:<@NatyMoraes, @ClaudiaStrm, @Marcos012> Arruma conflito com a master #10

--- src/routes/medicamento/edit.js
+++ src/routes/medicamento/edit.js
@@ -20,5 +20,5 @@ module.exports = (Medicamento, Prescricao, Acolhido) => (req, res) => {
           updateUrl: req.originalUrl,
           acolhido: medicamento.prescricao.acolhido
         })
-    })
+    }})
 }
