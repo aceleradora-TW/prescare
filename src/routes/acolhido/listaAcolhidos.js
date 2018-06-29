565c9752b6c9632d5f3c2c5dea466041fba9c920
diovanemendes <diovane.m.mendes@gmail.com>
2018-06-29 11:28:56 -0300

    :rocket: <@ilizakoski, @murilobauerc, @diovanemendes> colocando data e hora

--- src/routes/acolhido/listaAcolhidos.js
+++ src/routes/acolhido/listaAcolhidos.js
@@ -1,8 +1,9 @@
+const moment = require('moment')
 module.exports = (Acolhido, Prescricao) => (req, res) => {  
   return Acolhido.findAll({
     order: [['nome', 'ASC'], [{ model: Prescricao }, 'validade', 'DESC']],
     include: [{ model: Prescricao, required: false, attributes: ['validade', 'updated_at']}]
   }).then(acolhidos => {   
-    res.render('pages/listaAcolhidos', { acolhidos, prescricaos: acolhidos.prescricaos })
+    res.render('pages/listaAcolhidos', { acolhidos, prescricaos: acolhidos.prescricaos, moment: moment })
   }).catch(err => console.log(err))  
 }
