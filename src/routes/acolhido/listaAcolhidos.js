5d483b8be9b3fff6b931c92b50e68b8c48c25f1c
murilobauerc <panchobauer@gmail.com>
2018-06-29 14:16:43 -0300

    :art: <@murilobauerc, @ilizakoski> arruma teste

--- src/routes/acolhido/listaAcolhidos.js
+++ src/routes/acolhido/listaAcolhidos.js
@@ -1,9 +1,8 @@
-const moment = require('moment')
 module.exports = (Acolhido, Prescricao) => (req, res) => {  
   return Acolhido.findAll({
     order: [['nome', 'ASC'], [{ model: Prescricao }, 'validade', 'DESC']],
     include: [{ model: Prescricao, required: false, attributes: ['validade', 'updated_at']}]
   }).then(acolhidos => {   
-    res.render('pages/listaAcolhidos', { acolhidos, prescricaos: acolhidos.prescricaos, moment: moment })
+    res.render('pages/listaAcolhidos', { acolhidos, prescricaos: acolhidos.prescricaos })
   }).catch(err => console.log(err))  
 }
