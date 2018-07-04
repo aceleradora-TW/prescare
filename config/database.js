259faa8ed710260da17035c52a543b05dd5a5d98
diovanemendes <diovane.m.mendes@gmail.com>
2018-07-04 11:44:55 -0300

    :art: <@diovanemendes, @lindicell> Arruma banco

--- config/database.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    'url': process.env.DATABASE_URL ? process.env.DATABASE_URL : 'postgres://postgres:prescare@localhost:5432/prescare'
-}
---
+++ database.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize')
+const {DATABASE_URL} = require('./settings.js')
+
+module.exports = {
+  connect() {
+    return new Sequelize(DATABASE_URL, {
+      dialect: 'postgres',
+      define: {
+        underscored: true,
+        timestamps: false, 
+      }
+    })
+  }
+}
