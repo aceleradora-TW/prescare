b0f0d95722d92a28267f626f3a6ddf9ede878ca2
ghidorsi <ghidorsi@thoughtworks.com>
2018-07-04 12:54:56 -0300

    Criando middleware para save de prescricao após alterar arquivos relativos

--- src/routes/medicamento/create.js
+++ src/routes/medicamento/create.js
@@ -1,16 +1,9 @@
-module.exports = (Medicamento, Prescricao) => (req, res) => {
-    return Prescricao.findOne({
-        where: {
-            id: req.params.prescricao_id
-        }
-    }).then(prescricao => {
-        Medicamento.create({
-            prescricao_id: prescricao.id
-        }).then(medicamento => {
-            prescricao.changed('updated_at', true)
-            prescricao.save().then(_ => {
-                res.redirect(req.originalUrl + "/" + medicamento.id + "/edit");
-            })
-        })
+module.exports = Medicamento => (req, res, next) => {
+    return Medicamento
+    .create({
+        prescricao_id: req.params.prescricao_id
+    }).then(medicamento => {
+        res.redirect(req.originalUrl + '/' + medicamento.id + '/edit')
+        next()
     })
 }
