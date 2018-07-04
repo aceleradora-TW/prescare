5db4f38d20664fc44fcc0237fdb7de9f41def758
ClaudiaStrm <s_moura@hotmail.com>
2018-07-04 13:12:18 -0300

    :art: <@ClaudiaStrm> retira link de inserção de acolhido para farmaceutica

--- src/routes/acolhido/listaAcolhidos.js
+++ src/routes/acolhido/listaAcolhidos.js
@@ -1,8 +1,12 @@
 module.exports = (Acolhido, Prescricao) => (req, res) => {  
   return Acolhido.findAll({
     order: [['nome', 'ASC'], [{ model: Prescricao }, 'validade', 'DESC']],
-    include: [{ model: Prescricao, required: false, attributes: ['validade', 'updated_at']}]
+    include: [{ 
+        model: Prescricao, 
+        required: false, 
+        attributes: ['validade', 'updated_at']
+      }]
   }).then(acolhidos => {   
-    res.render('pages/listaAcolhidos', { acolhidos, prescricaos: acolhidos.prescricaos })
+    res.render('pages/listaAcolhidos', { acolhidos, prescricaos: acolhidos.prescricaos, tipoDoUsuario: req.user.tipo })
   }).catch(err => console.log(err))  
 }
