92a76e6d46f0a36582dd17f71a3617f75f020f25
laurascclaro <laurascclaro@hotmail.com>
2018-07-05 13:48:12 -0300

    :bug: <@laurascclaro, @murilobauerc, @marcos012> arruma bug na data

--- public/js/i18n/datepicker-pt-br.js
+++ public/js/i18n/datepicker-pt-br.js
@@ -33,7 +33,6 @@ datepicker.regional[ "pt-BR" ] = {
 	dayNamesShort: [ "Dom","Seg","Ter","Qua","Qui","Sex","Sáb" ],
 	dayNamesMin: [ "Dom","Seg","Ter","Qua","Qui","Sex","Sáb" ],
 	weekHeader: "Sm",
-	dateFormat: "dd/mm/yy",
 	firstDay: 0,
 	isRTL: false,
 	showMonthAfterYear: false,
