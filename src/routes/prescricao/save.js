ccdd27b63696d03aeb6935eabdd571fbff389230
laurascclaro <laurascclaro@hotmail.com>
2018-07-09 14:04:08 -0300

    :art: <@laurascclaro, @ilizakoski> atualiza com a master

--- src/routes/prescricao/save.js
+++ src/routes/prescricao/save.js
@@ -5,6 +5,7 @@ module.exports = Prescricao => (req, res) => {
   return Prescricao
   .create({
     acolhido_id: req.params.acolhido_id,
+    usuario: req.user.tipo,
     validade: moment(req.body.validade, FORMATO_DATA),
     data: new Date().getTime()
   }).then(prescricao => {
