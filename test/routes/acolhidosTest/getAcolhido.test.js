4ebff7f47b66e4b29a8267399e691621512dd2c9
othman853 <anuar.yasser@gmail.com>
2018-08-02 14:17:08 -0300

    #107 @othman853: Refatora testes unitários de routes/acolhido/get

--- test/routes/acolhidosTest/getAcolhido.test.js
+++ test/routes/acolhidosTest/getAcolhido.test.js
@@ -1,43 +1,68 @@
 const acolhidoRoutes = require('../../../src/routes/acolhido/get')
 
 describe('Quando acesso acolhido', () => {
-  it('Deve mostrar pagina com prescrição e suas abas atualizaveis', (done) => {
-    const Acolhido = {
-      findOne: jest.fn()
-    }
 
-    const model = {
-      include: jest.fn()
+  const Acolhido = {
+    findOne: jest.fn()
+  }
+
+  const Prescricao = {whatever: 'whatever'}
+
+  const req = {
+    params: {
+      acolhido_id: 1,
+      prescricao_id: 1
+    },
+    urlOriginal: '',
+    user: {
+      tipo: 'clinica'
     }
-    const req = { params: { acolhido_id: 1, prescricao_id: 1 }, urlOriginal: '', user: { tipo: 'clinica' } }
-    const res = { render: jest.fn() }
+  }
+
+  const res = { render: jest.fn() }
+
+  const next = jest.fn()
+
+  beforeEach(() => {
+    Acolhido.findOne.mockReset()
+    res.render.mockReset()
+    next.mockReset()
+  })
+
+  it('Deve mostrar pagina com prescrição e suas abas atualizaveis', (done) => {
 
     const acolhido = {
       nome: 'Leo',
-      id: '1',
-      nascimento: 'Luna',
-      peso: 'Luna',
-      alergias: 'Luna',
-      viaAlimentacao: 'Luna',
       prescricaos: [{
         id: 1,
         data: '2018-06-15',
         validade: '2018-06-08',
-        acolhido_id: '1',
+        acolhido_id: '1'
       }]
     }
 
-    const tipoDoUsuario = req.user.tipo
-    const updateUrl = req.urlOriginal
-    const prescricaoId = req.params.prescricao_id
-    const prescricaos = acolhido.prescricaos
-
     Acolhido.findOne.mockResolvedValue(acolhido)
-    model.include.mockResolvedValue(prescricaos)
 
-    acolhidoRoutes(Acolhido, model)(req, res)
-      .then(() => expect(Acolhido.findOne).toBeCalledWith({ 'where': { 'id': req.params.acolhido_id }, 'include': [{ model, 'required': false, 'where': { 'acolhido_id': req.params.acolhido_id } }] }))
-      .then(() => expect(res.render).toBeCalledWith('pages/infoAcolhido', { acolhido, tipoDoUsuario, prescricaoId, prescricaos, updateUrl}))
+    acolhidoRoutes(Acolhido, Prescricao)(req, res, next)
+      .then(() => expect(Acolhido.findOne).toBeCalledWith({
+        where: {
+          id: req.params.acolhido_id
+        },
+        include: [{
+          model: Prescricao,
+          required: false,
+          where: {
+            acolhido_id: req.params.acolhido_id
+          }
+        }]
+      }))
+      .then(() => expect(res.render).toBeCalledWith('pages/infoAcolhido', {
+        acolhido,
+        tipoDoUsuario: req.user.tipo,
+        prescricaoId: req.params.prescricao_id,
+        prescricaos: acolhido.prescricaos,
+        updateUrl: req.urlOriginal
+      }))
       .then(done)
       .catch(done)
   })
