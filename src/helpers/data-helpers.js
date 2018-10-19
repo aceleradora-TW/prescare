05c43b0fc64af4197ea17ed147ff5b1f0e3dedcc
othman853 <anuar.yasser@gmail.com>
2018-10-19 02:51:31 -0300

    @othman853: Redesenha lista de acolhidos e formulario de login

--- src/helpers/data-helpers.js
+++ src/helpers/data-helpers.js
@@ -4,10 +4,11 @@ const formatoData = 'DD/MM/YYYY'
 
 const formatarData = ({ data, formato = 'DD/MM/YYYY', fallback = '' }) => data ? moment(data).format(formato) : fallback
 const converterData = data => moment(data, formatoData)
-
+const idadeEmAnos = data => moment().diff(moment(data, formatoData), 'years')
 
 module.exports = app => (req, res, next) => {
   app.locals.formatarData = formatarData
   app.locals.converterData = converterData
+  app.locals.idadeEmAnos = idadeEmAnos
   next()
 }
